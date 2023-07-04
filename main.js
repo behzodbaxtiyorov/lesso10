d706f97137acb418f588b8fe3176f388b0c00547
behzodbaxtiyorov <behzodbaxtiyorov6@gmail.com>
2023-07-04 14:41:10 +0500

    uyga vazifa 10

---
+++ main.js
@@ -0,0 +1,173 @@
+class students {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+}
+
+const Ali = new students('Ali',20);
+const Behzod = new students('Behzod',21);
+
+
+
+
+
+class Device {
+    constructor(name, company) {
+      this.name = name;
+      this.company = company;
+    }
+}
+
+const iwatch = new Device ('Aplle', 'iwatch');
+
+
+
+
+
+class Telefon {
+    constructor(name, number) {
+      this.name = name;
+      this.number = number;
+    }
+}
+
+const iphone = new Telefon ('Aplle', '881310107');
+
+
+
+
+
+
+
+class Noutbook {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+}
+
+const noutbook = new Noutbook ('victus', 2023);
+
+
+
+
+
+
+class Moshina {
+    constructor(company, age) {
+      this.company = company;
+      this.age = age;
+    }
+}
+
+const moshina = new Moshina ('BMW', 2019);
+
+
+
+
+
+
+class city {
+    constructor(name, country) {
+      this.name = name;
+      this.country = country;
+    }
+}
+
+const moscow = new city ('Moscow', 'Russia');
+
+
+
+
+
+
+class Holodilnik {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+}
+
+const holodilnik = new Holodilnik ('Artel', 2022);
+
+
+
+
+
+
+class Kondisioner {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+}
+
+const kondisioner = new Kondisioner ('Artel', 2020);
+
+
+
+
+
+class Oquvmarkaz {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+}
+
+const oquvmarkaz = new Oquvmarkaz ('Najot', 2018);
+
+
+
+
+
+class Maktab {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+}
+
+const maktab = new Maktab ('Prezident', 2018);
+
+
+
+
+
+
+class Uzbekistan {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+}
+
+const uzbekistan = new Uzbekistan ('Uzbekistan', 1991);
+
+
+
+
+
+class Magazin {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+}
+
+const magazin = new Magazin ('Produkt', 2023);
+
+
+
+
+
+
+class Car {
+    constructor(name, year) {
+      this.name = name;
+      this.year = year;
+    }
+  }
+
+  const car = new Car ('Mers', 2019);
